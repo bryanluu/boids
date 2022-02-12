63abc46c2342b4b2169634ef0cf9c1527827711e
Bryan Luu <loc.bryan.luu@gmail.com>
2022-02-11 18:46:47 -0800

    :sparkles: add speed constraints

--- boids.py
+++ boids.py
@@ -5,6 +5,9 @@ import numpy as np
 import utilities
 
 class Boid(utilities.DrawSprite):
+  MIN_SPEED = 5
+  MAX_SPEED = 10
+
   def __init__(self, pos, vel):
     # Call the parent class (Sprite) constructor
     pygame.sprite.Sprite.__init__(self)
@@ -16,6 +19,7 @@ class Boid(utilities.DrawSprite):
     self.image = pygame.Surface([self.width, self.width], flags=pygame.SRCALPHA)
 
   def update(self):
+    self.constrain_speed()
     self.position += self.velocity
     self.rect = pygame.Rect(self.position.X, self.position.Y, self.width, self.width)
     self.image.fill(self.color)
@@ -23,4 +27,11 @@ class Boid(utilities.DrawSprite):
   @property
   def color(self):
     return *colors.WHITE, self.position.Z
+
+  def constrain_speed(self):
+    speed = abs(self.velocity)
+    if speed > Boid.MAX_SPEED:
+      self.velocity = (self.velocity * Boid.MAX_SPEED) / speed
+    if speed < Boid.MIN_SPEED:
+      self.velocity = (self.velocity * Boid.MIN_SPEED) / speed
 
